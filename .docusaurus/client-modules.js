4ca3e08950a2c2f4fe7d856c230635d4ec7d7e3e
Helder Vasconcelos <helder@taikai.network>
2021-01-14 12:44:07 +0000

    Generated Data

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/heldervasconcelos/Desktop/Taikai/Code/taikai-docs/taikai-docs/src/css/custom.css"),
-];
