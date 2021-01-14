f19ebe971f62f3ec3ce9590efa63d427d1143a47
Helder Vasconcelos <helder@taikai.network>
2021-01-14 12:44:32 +0000

    Fixe

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/taikai/taikai-docs/edit/master/website/',
           routeBasePath: '/'
         },
         blog: {
